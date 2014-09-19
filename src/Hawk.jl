e3d99d4cd1d72dd3e0571dc6270a45c4891fd2a2
Daniel Høegh <dhoegh91@gmail.com>
2014-09-19 09:07:51 +0200

    This is init commit.

--- src/Hawk.jl
+++ src/Hawk.jl
@@ -1,5 +1,16 @@
 module Hawk
 
-# package code goes here
+export @constants
+macro constants(exprs)
+    con=symbol("const")
+    for (i,exp) in enumerate(exprs.args)
+        if exp.head==:(=)
+            out = Expr(con)
+            push!(out.args,:($exp))
+            exprs.args[i] = out
+        end
+    end
+    return esc(exprs)
+end
 
 end # module
